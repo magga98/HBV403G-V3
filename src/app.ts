ec0ac7fa061b6a2610e9c4e9a58fbf126e1803ba
magga98 <margretdis@hotmail.com>
2023-03-10 09:41:54 +0000

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
 import { router } from './routes/api.js';
 
 dotenv.config();
@@ -7,6 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
 
 const port = 3000;
@@ -15,21 +16,20 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
-function notFoundHandler(req: Request, res: Response, next: NextFunction) {
-  console.warn('Not found', req.originalUrl);
-  res.status(404).json({ error: 'Not found'});
-}
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: 'not found' });
+});
 
-function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  console.error(err);
-  /*
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-    return res.status(400).json({ error: 'Invalid json'});
+app.use((err: Error, req:Request, res: Response, next: NextFunction) => {
+  if (
+    err instanceof SyntaxError && 
+    'status' in err && 
+    err.status === 400 && 
+    'body' in err
+  ) {
+    return res.status(400).json({ error: 'invalid json' });
   }
-  */
 
+  console.error('error handling route', err);
   return res.status(500).json({ error: 'Internal server error'});
-}
-
-app.use(notFoundHandler);
-app.use(errorHandler);
+});
